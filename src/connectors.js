300e5bb4a3284015fc589e52ec23d767e0c4a50f
Brad <brad@xovation.com>
2018-04-29 12:16:37 -0600

    Replace recursive CTE with local processing

--- src/connectors.js
+++ src/connectors.js
@@ -47,35 +47,74 @@ const Resort = {
             `);
         return result.recordset[0];
     },
+    //getWaitTimePeriods: async (waitTimeDate, args, context) => {
+    //    const result = await context.db.request()
+    //        .input('resortID', mssql.Int, waitTimeDate.resortID)
+    //        .input('date', mssql.Date, waitTimeDate.date)
+    //        .query(`
+    //            select  timestamp, liftID, seconds
+    //            from	WaitTimeDateUplift
+    //            where	ResortID = @resortID 
+    //            and     WaitTimeDate = @date
+    //            order by timestamp
+    //            OPTION (RECOMPILE)
+    //        `);
+    //    let timePeriod = null;
+    //    const data = result.recordset.reduce((waitTimePeriods, waitTime) => {
+    //        if (!timePeriod || timePeriod.timestamp != waitTime.timestamp) {
+    //            //depends on a pre-ordered list of waitTimes
+    //            timePeriod = {
+    //                timestamp: waitTime.timestamp,
+    //                waitTimes: [],
+    //            };
+    //            waitTimePeriods.push(timePeriod);
+    //        }
+    //        timePeriod.waitTimes.push({
+    //            liftID: waitTime.liftID,
+    //            seconds: waitTime.seconds,
+    //        });
+    //        return waitTimePeriods;
+    //    }, []);
+    //    return data;
+    //},
     getWaitTimePeriods: async (waitTimeDate, args, context) => {
+        //recursive CTE used in above view too slow (~450ms)
         const result = await context.db.request()
             .input('resortID', mssql.Int, waitTimeDate.resortID)
             .input('date', mssql.Date, waitTimeDate.date)
             .query(`
-                select  timestamp, liftID, seconds
-                from	WaitTimeDateUplift
-                where	ResortID = @resortID 
-                and     WaitTimeDate = @date
-                order by timestamp
-                OPTION (RECOMPILE)
+	            select  LocalMinutes / 15 as timeperiod, u.liftID, WaitSeconds as seconds
+	            from	Uplift u 
+	            join	Lift l on l.LiftID = u.LiftID
+                where   l.ResortID = @resortID
+                and     u.LocalDate = @date
+                order by LocalMinutes
             `);
-        let timePeriod = null;
-        const data = result.recordset.reduce((waitTimePeriods, waitTime) => {
-            if (!timePeriod || timePeriod.timestamp != waitTime.timestamp) {
-                //depends on a pre-ordered list of waitTimes
-                timePeriod = {
-                    timestamp: waitTime.timestamp,
-                    waitTimes: [],
-                };
-                waitTimePeriods.push(timePeriod);
-            }
-            timePeriod.waitTimes.push({
-                liftID: waitTime.liftID,
-                seconds: waitTime.seconds,
+        const waitTimes = result.recordset;
+        const periods = waitTimes.map(uplift => uplift.timeperiod);
+        const startPeriod = Math.min(...periods);
+        const endPeriod = Math.max(...periods);
+        const waitTimePeriods = [];
+        let cumulativeWaitTimes = [];
+        for (let period = startPeriod; period <= endPeriod; period++) {
+            //load the wait times for the period
+            const specificWaitTimes = waitTimes
+                .filter(waitTime => waitTime.timeperiod === period)
+                .map(waitTime => ({
+                    liftID: waitTime.liftID,
+                    seconds: waitTime.seconds,
+                }));
+            //accumulate wait times, favoring the most recent one for a lift: reload cumulative wait times 
+            //with the most recent lift times, plus any previous lift times not found in the specific period
+            cumulativeWaitTimes = specificWaitTimes
+                .concat(cumulativeWaitTimes.filter(cumulativeWaitTime =>
+                    !specificWaitTimes.find(specificWaitTime => specificWaitTime.liftID === cumulativeWaitTime.liftID)));
+            waitTimePeriods.push({
+                timestamp: period * 15 * 60,
+                waitTimes: cumulativeWaitTimes,                
             });
-            return waitTimePeriods;
-        }, []);
-        return data;
+        }
+        return waitTimePeriods;
     },
     getWaitTimeDates: (resort, args, context) => context.dataLoaders.waitTimeDatesByResortIDs.load(resort.id),
     getLifts: (resort, args, context) => context.dataLoaders.liftsByResortIDs.load(resort.id),
